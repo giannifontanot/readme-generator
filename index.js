6cd56d4b7d1f6c1658dd488d6a58131fc21116c9
giannifontanot <giannifontanot@gmail.com>
2021-07-23 07:43:42 -0500

    Updated validations for blank responses.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Required libraries: INQUIRER and FILE SYSTEM
 const inquirer = require('inquirer');
 const fs = require('fs');
+const tools = require('./lib/tools');
 
+//Paint the screen
+tools.clearPage();
 // Begin prompt - Inquirer
 inquirer.prompt([
     {
@@ -11,12 +14,22 @@ inquirer.prompt([
         message: "What is the status of the project?",
         choices: ["Stable", "Buggy"],
         default: "Stable",
+        validate: (status) => {
+            return true;
+        },
     },
     {
         // Title of the project
         name: "title",
         type: "input",
         message: "Title of your project",
+        validate: (title) => {
+            if (title.toString().length <= 0) {
+                return "Title cannot be empty.";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // License used
@@ -25,71 +38,151 @@ inquirer.prompt([
         message: "Choose the license",
         choices: ["Apache", "BSD", "GPL", "MIT"],
         default: "MIT",
+        validate: (license) => {
+            return true;
+        },
     },
     {
         // Description of the project
         name: "description",
         type: "input",
         message: "A brief description of what this project does and who it's for",
+        validate: (description) => {
+            if (description.toString().length <= 0) {
+                return "Description cannot be empty.";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // Picture used
         name: "picture",
         type: "input",
         message: "insert the name of the picture",
+        validate: (picture) => {
+            if (picture.toString().length <= 0) {
+                return "Please provide a picture";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // Features 1
         name: "feature1",
         type: "input",
         message: "Write feature 1",
+        validate: (feature1) => {
+            if (feature1.toString().length <= 0) {
+                return "Please provide feature";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // Features 2
         name: "feature2",
         type: "input",
         message: "Write feature 2",
+        validate: (feature2) => {
+            if (feature2.toString().length <= 0) {
+                return "Please provide feature";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // Features 3
         name: "feature3",
         type: "input",
         message: "Write feature 3",
+        validate: (feature3) => {
+            if (feature3.toString().length <= 0) {
+                return "Please provide feature";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // Features 4
         name: "feature4",
         type: "input",
         message: "Write feature 4",
-    },
-    {
-        // Name of the GitHub repository
-        name: "githubrepo",
-        type: "input",
-        message: "What is the GitHub repository?",
-    }, {
-        // Live Link in the GitHub repository
-        name: "livelink",
-        type: "input",
-        message: "What is the Live Link of the project?",
-    },
-    {
-        // Author email
-        name: "email",
-        type: "input",
-        message: "provide the email to get support",
+        validate: (feature4) => {
+            if (feature4.toString().length <= 0) {
+                return "Please provide feature";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // GitHub account
         name: "githubaccount",
         type: "input",
         message: "What is your Github account?",
+        validate: (githubaccount) => {
+            if (githubaccount.toString().length <= 0) {
+                return "Please provide a GitHub account";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        // Name of the GitHub repository
+        name: "githubrepo",
+        type: "input",
+        message: "What is the GitHub repository?",
+        validate: (githubrepo) => {
+            if (githubrepo.toString().length <= 0) {
+                return "Please provide a GitHub repository for the project";
+            } else {
+                return true;
+            }
+        },
+    }, {
+        // Live Link in the GitHub repository
+        name: "livelink",
+        type: "input",
+        message: "What is the Live Link of the project?",
+        validate: (livelink) => {
+            if (livelink.toString().length <= 0) {
+                return "Please provide a live link for the project";
+            } else {
+                return true;
+            }
+        },
+    },
+    {
+        // Author email
+        name: "email",
+        type: "input",
+        message: "provide the email to get support",
+        validate: (email) => {
+            if (email.toString().length <= 0) {
+                return "Please provide an email account";
+            } else {
+                return true;
+            }
+        },
     },
     {
         // LinkedIn account
         name: "linkedinaccount",
         type: "input",
         message: "What is your LinkedIn account?",
+        validate: (linkedinaccount) => {
+            if (linkedinaccount.toString().length <= 0) {
+                return "Please provide a LinkedIn account";
+            } else {
+                return true;
+            }
+        },
     },
 ]).then(data => {
     // Empty string variable to gather all the data
@@ -126,7 +219,7 @@ inquirer.prompt([
     sReadme += "\n" + data.description;
     // Get the picture
     sReadme += "\n" + "## Demo"
-    sReadme += "\n" + "!["+data.picture+"](" + data.picture + ")";
+    sReadme += "\n" + "![" + data.picture + "](" + data.picture + ")";
     // Get 4 features of the project
     sReadme += "\n" + "## Features";
     sReadme += "\n" + "- " + data.feature1;
@@ -135,20 +228,20 @@ inquirer.prompt([
     sReadme += "\n" + "- " + data.feature4;
     // Get GitHub repository
     sReadme += "\n" + "## GitHub Repository";
-    sReadme += "\n" + "[https://github.com/" + data.githubaccount + "/" + data.githubrepo+"/](https://github.com/" + data.githubaccount + "/" + data.githubrepo+"/)";
+    sReadme += "\n" + "[https://github.com/" + data.githubaccount + "/" + data.githubrepo + "/](https://github.com/" + data.githubaccount + "/" + data.githubrepo + "/)";
     // Get Live Link
     sReadme += "\n" + "## Live Link";
-    sReadme += "\n" + "[https://" + data.githubaccount + ".github.io/" + data.livelink+"](https://" + data.githubaccount + ".github.io/" + data.livelink+")";
+    sReadme += "\n" + "[https://" + data.githubaccount + ".github.io/" + data.livelink + "](https://" + data.githubaccount + ".github.io/" + data.livelink + ")";
     // Get email for Support
     sReadme += "\n" + "## Support";
-    sReadme += "\n" + "If you need help with this project, please write to: [" + data.email+"](" + data.email+")";
+    sReadme += "\n" + "If you need help with this project, please write to: [" + data.email + "]((https://mailto:" + data.email + ")";
     // Get Author of the project
     sReadme += "\n" + "## Authors";
-    sReadme += "\n" + " - [@"+data.githubaccount+"](https://www.github.com/"+data.githubaccount+")";
+    sReadme += "\n" + " - [@" + data.githubaccount + "](https://www.github.com/" + data.githubaccount + ")";
     // LinkedIn and GitHub links
     sReadme += "\n" + "## 🔗 Links";
-    sReadme += "\n" + "[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/"+data.github+"/)";
-    sReadme += "\n" + "[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/"+data.linkedinaccount+"/)";
+    sReadme += "\n" + "[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/" + data.github + "/)";
+    sReadme += "\n" + "[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/" + data.linkedinaccount + "/)";
     // We write the results
     writeReadmeFile(sReadme);
 });
@@ -157,6 +250,7 @@ inquirer.prompt([
 function callbackWriteFile(err) {
     err ? console.log(err) : console.log('success');
 }
+
 // We write the results
 function writeReadmeFile(pText) {
     // Name of the file is always README.md
